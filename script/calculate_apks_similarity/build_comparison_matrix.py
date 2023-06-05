279999d5295147f0ad22b2a4193b1558b7561753
Valery Petrov <valeryvpetrov.dev@gmail.com>
2023-06-05 20:07:39 +0300

    Change GED normalization

--- script/calculate_apks_similarity/build_comparison_matrix.py
+++ script/calculate_apks_similarity/build_comparison_matrix.py
@@ -1,10 +1,11 @@
-from networkx.algorithms.similarity import graph_edit_distance
-import networkx as nx
-from concurrent.futures import ThreadPoolExecutor
-from multiprocessing.pool import Pool
-from multiprocessing import current_process
-import numpy as np
 import threading
+from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import current_process
+from multiprocessing.pool import Pool
+
+import networkx as nx
+import numpy as np
+from networkx.algorithms.similarity import graph_edit_distance
 
 
 def build_comparison_matrix(
@@ -48,24 +49,12 @@ def calculate_similarity_two_dots(
 ) -> float:
     global ins_block_sim_threshold_glob
     ins_block_sim_threshold_glob = ins_block_sim_threshold
-
-    empty_graph = nx.empty_graph(0, create_using=nx.DiGraph())
-    edit_distance_1_2 = graph_edit_distance(
+    ged = graph_edit_distance(
         dots_1, dots_2,
         node_match=calculate_similarity_ins_block,
         timeout=ged_timeout_sec
     )
-    edit_distance_1_0 = graph_edit_distance(
-        dots_1, empty_graph,
-        node_match=calculate_similarity_ins_block,
-        timeout=ged_timeout_sec
-    )
-    edit_distance_2_0 = graph_edit_distance(
-        dots_2, empty_graph,
-        node_match=calculate_similarity_ins_block,
-        timeout=ged_timeout_sec
-    )
-    sim = 1 - (edit_distance_1_2 / (edit_distance_1_0 + edit_distance_2_0))
+    sim = normalize_ged(ged, dots_1, dots_2)
     print("Process={}, Thread={} — dots_1={}, dots_2={}, sim={}".format(
         current_process().name,
         threading.get_ident(),
@@ -103,3 +92,26 @@ def clean_ins_block(ins_block: str) -> list:
     ins = ins.split("\\l")
     ins.pop()
     return ins
+
+
+def normalize_ged(ged: int, dots_1, dots_2) -> float:
+    dots_1_size = dots_1.number_of_nodes() + dots_1.number_of_edges()
+    dots_2_size = dots_2.number_of_nodes() + dots_2.number_of_edges()
+    max_size = max(dots_1_size, dots_2_size)
+    return 1 - ged / max_size
+
+
+def normalize_ged_via_empty_graph(ged: int, dots_1, dots_2, ged_timeout_sec: int) -> float:
+    empty_graph = nx.empty_graph(0, create_using=nx.DiGraph())
+
+    edit_distance_1_0 = graph_edit_distance(
+        dots_1, empty_graph,
+        node_match=calculate_similarity_ins_block,
+        timeout=ged_timeout_sec
+    )
+    edit_distance_2_0 = graph_edit_distance(
+        dots_2, empty_graph,
+        node_match=calculate_similarity_ins_block,
+        timeout=ged_timeout_sec
+    )
+    return 1 - (ged / (edit_distance_1_0 + edit_distance_2_0))
